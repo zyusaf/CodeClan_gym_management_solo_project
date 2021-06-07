86fe9d0fe79fa0c828af454e3cedfa5585862250
Zaki Yusaf <zakiyusaf@hotmail.co.uk>
2021-06-07 12:02:50 +0100

    Changes made to routes in members controller and addition of form in new.html

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -5,13 +5,45 @@ import repositories.member_repository as member_repository
 
 members_blueprint = Blueprint("members", __name__)
 
-@members_blueprint.route("/members")
-def members():
-    members = member_repository.select_all()
-    return render_template("members/index.html", members = members)
+# #INDEX
+# @members_blueprint.route("/members")
+# def members():
+#     members = member_repository.select_all()
+#     return render_template("members/index.html", members = members)
 
-@members_blueprint.route("/members/<id>")
-def show(id):
+#NEW
+@members_blueprint.route("/members")
+def new_member():
+    members = member_repository.select_all()
+    return render_template("members/new.html", members = members)
+
+#CREATE
+@members_blueprint.route("/members", methods=["POST"])
+def create_member():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    new_member = Member(first_name, last_name)
+    member_repository.save(new_member)
+    return redirect("/members")
+
+#EDIT
+@members_blueprint.route("/members/<id>/edit")
+def edit_member():
     member = member_repository.select(id)
-    sessions = member_repository.sessions(member)
-    return render_template("members/show.html", member = member, sessions = sessions)
+    return render_template("members/edit.html", member = member)
+
+#UPDATE
+@members_blueprint.route("/members/<id>", methods=["POST"])
+def update_member(id):
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    member = Member(first_name, last_name, id)
+    member_repository.update(member)
+    return redirect("/members")
+
+#SHOW
+# @members_blueprint.route("/members/<id>")
+# def show(id):
+#     member = member_repository.select(id)
+#     sessions = member_repository.sessions(member)
+#     return render_template("members/show.html", member = member, sessions = sessions)
